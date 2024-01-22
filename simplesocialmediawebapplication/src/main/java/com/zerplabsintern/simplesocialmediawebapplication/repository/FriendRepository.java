c9a359cd4a9d5a90fa54e1c142867761869fe891
unknown <davidjrmeshach@gmail.com>
2024-01-22 08:19:04 +0530

    query for methods added

--- simplesocialmediawebapplication/src/main/java/com/zerplabsintern/simplesocialmediawebapplication/repository/FriendRepository.java
+++ simplesocialmediawebapplication/src/main/java/com/zerplabsintern/simplesocialmediawebapplication/repository/FriendRepository.java
@@ -3,15 +3,19 @@ package com.zerplabsintern.simplesocialmediawebapplication.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.zerplabsintern.simplesocialmediawebapplication.entity.Friend;
 
 public interface FriendRepository extends JpaRepository<Friend, Long > {
 
+    @Query(value = "Select * from Friend where user_id = ?1", nativeQuery = true)
     List<Friend> findByfUser_Id(Long id);
 
+    @Query(value = "Select * from Friend where user_id = ?1", nativeQuery = true)
     boolean existsByfUser_Id(Long userId);
 
+    @Query(value = "Select friend_id from Friend where user_id = ?1", nativeQuery = true)
     Friend findFriendIdByfUser_Id(Long userId);
     
 }
--- simplesocialmediawebapplication/src/main/java/com/zerplabsintern/simplesocialmediawebapplication/repository/LikeRepository.java
+++ simplesocialmediawebapplication/src/main/java/com/zerplabsintern/simplesocialmediawebapplication/repository/LikeRepository.java
@@ -3,14 +3,17 @@ package com.zerplabsintern.simplesocialmediawebapplication.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.zerplabsintern.simplesocialmediawebapplication.entity.Likes;
 import com.zerplabsintern.simplesocialmediawebapplication.entity.Post;
 
 public interface LikeRepository extends JpaRepository<Likes, Long> {
 
+    @Query(value = "Select * from Likes where post_id = ?1", nativeQuery = true)
     List<Likes> findLikesBylPost(Post lPost);
 
+    @Query(value = "Select * from Likes where post_id = ?1", nativeQuery = true)
     boolean existsBylPostId(Long id);
     
 }
