7a238ca32add193d2f4ffab47308cbef2e29dbd9
DavidMeshachP <davidjrmeshach@gmail.com>
2024-02-01 18:58:22 +0530

    working on image compression

--- simplesocialmediawebapplication/src/main/java/com/zerplabsintern/simplesocialmediawebapplication/service/impl/PostImagesServiceImpl.java
+++ simplesocialmediawebapplication/src/main/java/com/zerplabsintern/simplesocialmediawebapplication/service/impl/PostImagesServiceImpl.java
@@ -1,5 +1,11 @@
 package com.zerplabsintern.simplesocialmediawebapplication.service.impl;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Base64;
+
+import javax.imageio.ImageIO;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +15,8 @@ import com.zerplabsintern.simplesocialmediawebapplication.exception.PostImagesSe
 import com.zerplabsintern.simplesocialmediawebapplication.repository.PostImagesRepository;
 import com.zerplabsintern.simplesocialmediawebapplication.repository.PostRepository;
 import com.zerplabsintern.simplesocialmediawebapplication.service.PostImagesService;
+
+import java.awt.image.BufferedImage;
 
 @Service
 public class PostImagesServiceImpl implements PostImagesService {
@@ -95,6 +103,35 @@ public class PostImagesServiceImpl implements PostImagesService {
         }
     }
 
-    
+    public String compressImage(String base64Image, int targetWidth, int targetHeight, float quality) throws Exception {
+        
+        byte[] imageData = Base64.getDecoder().decode(base64Image);
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(imageData);
+        BufferedImage image = ImageIO.read(bais);
+
+        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+        
+        resizedImage.createGraphics().drawImage(image.getScaledInstance(targetWidth, targetHeight, java.awt.Image.SCALE_SMOOTH), 0, 0, null);
+
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(resizedImage, "jpg", baos);
+        baos.flush();
+        byte[] compressedImageData = baos.toByteArray();
+        baos.close();
+
+        
+        String compressedBase64String = Base64.getEncoder().encodeToString(compressedImageData);
+
+        return compressedBase64String;
+
+    }
+
+    public int getSizeOfImage(String base64String) {
+        String base64Image = base64String;
+        byte[] decodedImage = Base64.getDecoder().decode(base64Image);
+        int imageSizeInBytes = decodedImage.length;
+        return imageSizeInBytes;
+    }
     
 }
